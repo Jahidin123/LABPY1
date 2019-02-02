ba3ed570e5b616310d630411dd39dafa18eebbfd
jahidin123 <jahidinsh19@mhs.pelitabangsa.ac.id>
2019-02-02 12:03:34 -0800

    program menentukan  bilangan terbesar dari 3 buah bilangan

---
+++ labpy1.py
@@ -0,0 +1,14 @@
+a = input ("masukan nilai a :")
+b = input ("masukan nilai b :")
+c = input ("masukan nilai c :")
+if (a>b):
+    if (a>c):
+        print (a)
+    else:
+        print (c)
+if (b>c):
+    if (b>c):
+        print  (b)
+else:
+    print  (c)
+
